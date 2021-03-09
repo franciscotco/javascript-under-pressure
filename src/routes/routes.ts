cd419cb27e589634f08c373b47b20d58c19fb9bb
François Moine <francois.moine@epitech.eu>
2021-03-09 11:58:31 +0100

    [*] bind js-test route to the app

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,18 +1,20 @@
 // Configs
 import HomeConfig from './home/config';
+import JavascriptTestConfig from './javascript-test/config';
 
 export interface IRoutes {
 	id: string,
 	name: string,
 	path: string,
 	exact: boolean,
-	Component: () => JSX.Element,
+	Component: any,
 	isEnable: () => boolean,
 	order: number
 }
 
 const ROUTES: IRoutes[] = [
-	HomeConfig
+	HomeConfig,
+	JavascriptTestConfig
 ];
 
 export function getRoutes (): IRoutes[] {
